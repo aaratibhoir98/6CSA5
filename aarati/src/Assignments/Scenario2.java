d956dce546f62731681bbc173e422905c360a071
Aarati <Aarati@LAPTOP-NV2GQMH2>
2023-05-31 11:44:41 +0530

    this is my fourth commit

---
+++ aarati/src/Assignments/Scenario2.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+import java.util.Set;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scenario2 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.fb.com");
+		Set<Cookie> allCookies = driver.manage().getCookies();
+		System.out.println(allCookies.size());
+		for(Cookie singleCooky:allCookies)
+			System.out.println(singleCooky.getName());
+		driver.manage().deleteCookieNamed("wd");
+		allCookies = driver.manage().getCookies();
+		System.out.println(allCookies.size());
+		for(Cookie singleCooky:allCookies)
+			System.out.println(singleCooky.getName());
+		
+	}
+
+}
